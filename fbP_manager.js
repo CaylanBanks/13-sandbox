2680423f6deb1c82b14c96d99602899318efdcbf
CaylanBanks <124848867+CaylanBanks@users.noreply.github.com>
2025-02-24 23:29:11 +0000

    proc read, proc write, error fixing

---
+++ fbP_manager.js
@@ -0,0 +1,109 @@
+/***************************************************************/
+// fbP_manager.js
+// Read & write to Firebase
+// Written by Caylan Banks Term 2 2024
+/**************************************************************/
+
+console.log("%c fbP_manager.js", "color:blue");
+
+fbP_initialize();
+/**************************************************************/
+// fbP_initialise()
+// Called by fbP_manager.js
+// Initilizes the database 
+// Input:  n/a
+// Return: n/a
+/**************************************************************/
+function fbP_initialize() {
+  console.log('fbP_initialise()');
+
+  if (typeof firebase === 'undefined') {
+      console.error("Firebase SDK not loaded properly.");
+      return;
+  }
+
+
+ const FIREBASECONFIG = {
+  apiKey: "AIzaSyBTotozJ_MhOZ3kesJwnMzgQ15oqUVHVHo",
+  authDomain: "comp-2025-caylan-banks.firebaseapp.com",
+  databaseURL: "https://comp-2025-caylan-banks-default-rtdb.firebaseio.com",
+  projectId: "comp-2025-caylan-banks",
+  storageBucket: "comp-2025-caylan-banks.firebasestorage.app",
+  messagingSenderId: "515643998293",
+  appId: "1:515643998293:web:4a5c7f1dd191b637e3d4dc",
+  measurementId: "G-92D1R4GW3P"
+};
+  console.log("firebase.initializeApp(FIREBASECONFIG);")
+
+  // Check if firebase has already been initialized
+  if (!firebase.apps.length) {
+      firebase.initializeApp(FIREBASECONFIG);
+      database = firebase.database();
+      console.log("Firebase initialized successfully.");
+  } else {
+      console.log("Firebase already initialized.");
+  }
+}
+
+/**************************************************************/
+// fbP_procLogin()
+// Process login result
+// Input: loginStatus, save object, user object, error object
+/**************************************************************/
+function fbP_procLogin(_loginStatus, _save, _user, _error) {
+  console.log("fbP_procLogin(): status = " + _loginStatus);
+
+  if (_loginStatus === "error") {
+    console.error("Login error: " + _error.code + " " + _error.message);
+    alert("Login failed: " + _error.message);
+  } else {
+    _save.uid = _user.uid;
+    _save.email = _user.email;
+    _save.displayName = _user.displayName;
+    _save.photoURL = _user.photoURL;
+    _save.emailVerified = _user.emailVerified;
+
+    sessionStorage.setItem("uid", _user.uid);
+    sessionStorage.setItem("email", _user.email);
+    sessionStorage.setItem("displayName", _user.displayName);
+    sessionStorage.setItem("photoURL", _user.photoURL);
+    sessionStorage.setItem("loginStatus", _loginStatus);
+
+    console.log("User details saved to sessionStorage: " + JSON.stringify(_save));
+
+  }
+
+}
+
+
+
+/**************************************************************/
+// fbP_procReadRec()
+// Process read result
+// Input: DB path, key, save object, error
+/**************************************************************/
+function fbP_procReadRec(_path, _key, _save, _error) {
+  console.log("fbP_procReadRec()");
+
+  if (_error) {
+    console.error(`Error reading from ${_path}/${_key}:`, _error);
+    alert("Read failed. See console log for details.");
+  } else {
+    console.log(`Successfully read from ${_path}/${_key}:`, _save);
+  }
+}
+/**************************************************************/
+// fbP_procWriteRec()
+// Process write result
+// Input: DB path, key, data, error
+/**************************************************************/
+function fbP_procWriteRec(_path, _key, _data, _error) {
+  console.log("fbP_procWriteRec()");
+
+  if (_error) {
+    console.error(`Error writing to ${_path}/${_key}:`, _error);
+    alert("Write failed. See console log for details.");
+  } else {
+    console.log(`Successfully wrote to ${_path}/${_key}`);
+  }
+}
---
+++ fb_io.js
@@ -0,0 +1,117 @@
+/***************************************************************/
+// fb_io.js
+//read & write to firebase
+// Written by Caylan Banks 2025
+/**************************************************************/
+
+console.log("%c fb_io.js", "color:blue");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyByB-WJsNPQEDK7SZ3dXrPMEUJ8aUpaaAg",
+    authDomain: "comp-2025-caylan-banks.firebaseapp.com",
+    databaseURL: "https://comp-2024-caylan-banks-default-rtdb.firebaseio.com",
+    projectId: "comp-2025-caylan-banks",
+    storageBucket: "comp-2025-caylan-banks.firebasestorage.app",
+    messagingSenderId: "727244724088",
+    appId: "1:727244724088:web:9c528f0677ca9f00bc5a8d",
+    measurementId: "G-H9NDB7QELM"
+  };
+
+/**************************************************************/
+// fb_login(_save,_procFunc)
+// Called by various
+// Input:  User logs in
+// Return: n/a
+/**************************************************************/
+ function fb_login(_save, _procFunc) {
+    console.log('fb_login() ');
+    firebase.auth().onAuthStateChanged(newLogin);
+  
+    function newLogin(user) {
+     let loginStatus;
+      if (user) {
+        // user is signed in, so call function to process login data
+        loginStatus = 'logged in';
+        _procFunc(loginStatus, user, _save, null);
+      } 
+      else {
+        // user NOT logged in, so redirect to Google login
+        loginStatus = 'logged out';
+        console.log('fb_login(): status = ' + loginStatus);
+  
+        var provider = new firebase.auth.GoogleAuthProvider();
+        /* To force Google sign to ask which account to use:
+        var provider = new firebase.auth.GoogleAuthProvider();
+        provider.setCustomParameters({
+          prompt: 'select_account'
+        });
+        */
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+          loginStatus = 'logged in via popup';
+          _procFunc(loginStatus, result.user, _save);
+        })
+        // Catch errors
+        .catch(function(error) {
+          loginStatus = 'error';
+          _procFunc(loginStatus, null, _save, error);
+        });
+      }
+    }
+
+  }
+
+/**************************************************************/
+// fb_readRec(_path,_key,_save,_procFunc)
+// Called by ?????
+// Read a record from a db
+// Input:  DB path and key, data to write,
+//Function to process the result of the read
+// Return: n/a
+/**************************************************************/
+//fb_readRec('admin', 'aaa', adminData);
+  function fb_readRec(_path, _key, _save, _procFunc) {
+    console.log("fb_readRec():" + _path + " key=" + _key);
+    firebase
+        .database()
+        .ref(_path + "/" + _key)
+        .once("value")
+        .then((snapshot) => {
+            const dbData = snapshot.val();
+            _procFunc(_path, _key, dbData, null);
+        })
+        .catch((_error) => {
+            console.error("Database read _error:", _error);
+            _procFunc(_path, _key, _save, _error);
+        });
+}
+
+
+/**********************************************************/
+// fb_writeRec(_path,_key,_data,_procFunc)
+// Called by ?????
+// Write rec to db
+// Input:  n/a
+// Return: n/a
+/**********************************************************/
+//fb_writeRec('admin', 'aaa', adminData, _procFunc)
+function fb_writeRec(_path, _key, _data, _procFunc) {
+    if (!_data.uid || !_data.displayName || !_data.email) {
+      console.error("Invalid data provided for writing.");
+      _procFunc(_path, _key, _data, new Error("Invalid data"));
+      return;
+    }
+  
+    console.log(
+      `fb_writeRec(): path= ${_path} key= ${_key} data= ${_data.uid}/${_data.displayName}/${_data.email}`
+    );
+  
+    firebase.database().ref(_path + "/" + _key).set(_data, (error) => {
+      if (error) {
+        _procFunc(_path, _key, _data, error);
+      } else {
+        _procFunc(_path, _key, _data, null);
+      }
+      console.log("fb_writeComplete: exit");
+    });
+  }
+
