38c42e5cdd008b03181f54615cdea951a6161ee2
CaylanBanks <124848867+CaylanBanks@users.noreply.github.com>
2025-03-30 22:05:32 +0000

    starting making lobby

---
+++ gtnLobby.js
@@ -0,0 +1,7 @@
+/***************************************************************/
+// gtnLobby.js
+// Written by Caylan Banks 2024/2025
+//read & write to firebase
+/**************************************************************/
+
+console.log("%c gtnLobby.js", "color:green");
