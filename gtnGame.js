aabd90367ee6fe8f151a4f734a4b6b451ef68b22
CaylanBanks <124848867+CaylanBanks@users.noreply.github.com>
2025-05-14 02:55:22 +0000

    join game

--- gtnGame.js
+++ gtnGame.js
@@ -1,6 +1,74 @@
 /***************************************************************/
-// fb_io.js
-// Written by Caylan Banks 2024/2025
-//read & write to firebase
-//Handle login
+//gtnGame.js
+//Written by Caylan Banks 2024/2025
+//Code for the Guess the Number game
 /**************************************************************/
+
+const userId = sessionStorage.getItem("user.uid");
+const displayName = sessionStorage.getItem("user.displayName");
+
+/**************************************************************/
+// gtn_createGame()
+// Called by gtnlobby.html
+// Input:  User clicks button
+// Return: Creates a game and adds it to the waitingGames list
+/**************************************************************/
+
+function gtn_createGame(gameId) {
+    console.log("%c gtn_createGame()", "color:green");
+
+    // Hide the "Create Game" button
+    const createGameButton = document.getElementById("createGameButton");
+    createGameButton.style.display = "none";
+
+    // Show the "Waiting for others to join" message
+    const waitingMessage = document.getElementById("waitingMessage");
+    waitingMessage.style.display = "block";
+
+
+
+    // Write to firebase that there is a waiting game
+    firebase.database().ref('/waitingGames/' + userId).set(displayName, (error) => {
+        if (error) {
+            console.error("Error creating game:", error);
+        } else {
+            console.log("Game created by:", displayName);
+        }
+    });
+
+    // Set game id to user id
+    const gameId = userId;
+
+    //Create the random number
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+    //write to the firebase the random number, the user id
+    // sets P1 as the active player with the next guess
+    firebase.database().ref('/gamesInProgress/' + gameId).set({
+    number: randomNumber,         
+    P1: userId,                   
+    activePlayer: "P1",           
+    [userId]: {                   
+        name: displayName,        
+        guess: "no guess yet",    
+        result: ""                
+    }
+}).then(() => {
+    console.log("Game setup complete. Waiting for a challenger...");
+});
+
+}
+
+/**************************************************************/
+// gtn_createGame()
+// Called by gtnlobby.html
+// Input:  User clicks button
+// Return: Creates a game and adds it to the waitingGames list
+/**************************************************************/
+function gtn_joinGame(gameId) { 
+    console.log("%c gtn_joinGame()", "color:green");
+
+
+
+
+}
